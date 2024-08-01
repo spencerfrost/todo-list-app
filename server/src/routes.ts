09eb95577462788c0dbcc56d199c38752a8a8b3f
spencerfrost <s.s.frost@gmail.com>
2024-07-31 23:24:29 -0400

    Fix the requests

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,8 +8,6 @@ router.post('/tasks', taskController.createTask);
 router.get('/tasks/:id', taskController.getTask);
 router.put('/tasks/:id', taskController.updateTask);
 router.delete('/tasks/:id', taskController.deleteTask);
-// completeTask
-router.put('/tasks/:id/complete', taskController.completeTask);
 
 // Add other routes here
 
