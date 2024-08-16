61c8f310e2d40a24ba3808eb34d56daea85102a4
Spencer Frost <34923439+spencerfrost@users.noreply.github.com>
2024-08-16 11:09:19 -0400

    Fix aliasing and importing

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,9 @@
+import App from 'App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './styles/index.css';
+
+import reportWebVitals from 'reportWebVitals';
+import 'styles/index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
