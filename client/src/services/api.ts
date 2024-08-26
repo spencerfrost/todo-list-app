b392a45f8505da16b286c96745c088ae43776d8f
Spencer Frost <34923439+spencerfrost@users.noreply.github.com>
2024-08-26 16:14:52 -0400

    chore: Update API_URL in client/src/services/api.ts to use environment variable

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 import { Task, UserSettings } from "services/types";
 
-const API_URL = process.env.REACT_APP_API_URL ?? '/api';
-
-console.log('Environment:', process.env.NODE_ENV);
-console.log('API URL:', process.env.REACT_APP_API_URL);
+const API_URL = process.env.NODE_ENV === "production" ? "https://taskmaster.mrspinn.ca/api" : "http://localhost:5000/api";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
